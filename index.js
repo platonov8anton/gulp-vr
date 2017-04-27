5415c7d79ef3d8b0324e842669cdc8cd67fd2713
Платонов Антон <platonov@bars-open.ru>
2017-04-27 10:28:15 +0300

    rename

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const getHash = require('./lib/getHash')
-const getFilename = require('./lib/getFilename')
+const hash = require('./lib/hash')
+const base64 = require('./lib/normalize/base64')
 const {readJsonSync, outputJson} = require('fs-extra')
 const {obj: through2} = require('through2')
 
@@ -22,23 +22,47 @@ exports.createManifest = (...paths) => Object.assign(
  * @param {String} [path] Path to file, to save the result
  * @return {Stream}
  */
-exports.digest = (path) => {
-  let manifest = {}
+exports.rename = ({
+    hash: {
+        algorithm = 'sha1',
+        encoding = 'base64'
+    },
+    rename
+}) => {
+
+  if (encoding !== 'hex' && encoding !== 'base64') {
+    throw new Error(`Hash encoding "${encoding}" not supported`)
+  }
 
   return through2(
-      (file, encoding, callback) => {
+      (file, enc, callback) => {
         if (file.isBuffer()) {
-          let base64 = getHash(file.contents)
-          let filename = getFilename(base64)
+          let stem = hash({algorithm, encoding, data: file.contents})
 
-          file._base = file.base
-          file._path = file.path
+          if (encoding === 'base64') {
+            stem = base64(stem)
+          }
 
-          file.stem = manifest[file.path] = filename
+          if (typeof rename === 'function') {
+            stem = rename(file.stem, stem)
+
+            if (typeof stem !== 'string') {
+              throw new TypeError('Base64 must be a string')
+            }
+            if (!stem) {
+              throw new Error('Base64 must be a string')
+            }
+          }
+
+          // file._base = file.base
+          // file._path = file.path
+
+          file.stem = stem
         }
 
         callback(null, file)
-      },
+      }
+      /*,
       (callback) => {
         if (path) {
           outputJson(path, manifest, callback)
@@ -46,5 +70,6 @@ exports.digest = (path) => {
           callback()
         }
       }
+      */
   )
 }
